1d027140833266189a91449266d4d36a7ab22a3b
Simon Zimmermann <simon@sizi.eu>
2017-01-27 13:02:41 +0100

    feat(SelectComponent): provide option for different label in menu selection

--- src/option-list.ts
+++ src/option-list.ts
@@ -20,7 +20,7 @@ export class OptionList {
         }
 
         this._options = options.map((option) => {
-            let o: Option = new Option(option.value, option.label);
+            let o: Option = new Option(option.value, option.label, option.labelMenu);
             if (option.disabled) {
                 o.disable();
             }
